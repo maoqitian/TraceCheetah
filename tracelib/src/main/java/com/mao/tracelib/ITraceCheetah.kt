3e37bac7f432a74e932ec8fe1754932bc8bfe753
maoqitian <maoqitian068@163.com>
2021-02-08 23:22:56 +0800

    update code

--- tracelib/src/main/java/com/mao/tracelib/ITraceCheetah.kt
+++ tracelib/src/main/java/com/mao/tracelib/ITraceCheetah.kt
@@ -3,7 +3,16 @@ package com.mao.tracelib
 /**
  *  author : maoqitian
  *  date : 2021/2/8 22:53
- *  description :
+ *  description : 耗时统计抽象接口
  */
 interface ITraceCheetah {
+
+    //统计开始
+    fun traceStart()
+    //统计结束
+    fun traceEnd()
+    //方法开始
+    fun methodStart(method: String?)
+    //方法结束
+    fun methodEnd(method: String?)
 }
--- tracelib/src/main/java/com/mao/tracelib/TraceCheetahTime.kt
+++ tracelib/src/main/java/com/mao/tracelib/TraceCheetahTime.kt
@@ -3,7 +3,8 @@ package com.mao.tracelib
 /**
  *  author : maoqitian
  *  date : 2021/2/8 23:20
- *  description :
+ *  description : 调用 ITraceCheetah 实例的方法
  */
 class TraceCheetahTime {
+
 }
