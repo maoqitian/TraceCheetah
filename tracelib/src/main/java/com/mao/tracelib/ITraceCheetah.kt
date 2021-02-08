b0920123f532321f0adec2526304db252fe8e157
maoqitian <maoqitian068@163.com>
2021-02-08 23:21:09 +0800

    add trace cheetah lib

---
+++ tracelib/src/main/java/com/mao/tracelib/ITraceCheetah.kt
@@ -0,0 +1,9 @@
+package com.mao.tracelib
+
+/**
+ *  author : maoqitian
+ *  date : 2021/2/8 22:53
+ *  description :
+ */
+interface ITraceCheetah {
+}
---
+++ tracelib/src/main/java/com/mao/tracelib/TraceCheetahTime.kt
@@ -0,0 +1,9 @@
+package com.mao.tracelib
+
+/**
+ *  author : maoqitian
+ *  date : 2021/2/8 23:20
+ *  description :
+ */
+class TraceCheetahTime {
+}
